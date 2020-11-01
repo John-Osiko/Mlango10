a523e3b04de4f2debb91b8dfbbf87f78e09e0deb
John-Osiko <johnmaxosiko@gmail.com>
2020-11-02 01:16:31 +0300

    Defined db model classes

--- mlangokumi_app/models.py
+++ mlangokumi_app/models.py
@@ -21,33 +21,125 @@ class Profile(models.Model):
     name = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     profile_pic = models.ImageField(upload_to='profile_pics/',blank=True, default='static/images/default.png')
     age = models.IntegerField(null=True)
-    contact = models.IntegerField(null=False, max_length=30)
+    contact = models.IntegerField(null=False)
     address = models.CharField(max_length=250)
     estate = models.ForeignKey('Neighbourhood', on_delete=models.CASCADE, blank=True, null=True)
     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
 
     class Meta:
-        db_table = 'profile'
+        db_table = 'profiles'
         ordering = ['name']
 
     def __str__(self):  # __unicode__ for Python 3
         return f'{self.name}'
 
-    @receiver(post_save, sender=User)
-    def save_profile(sender, instance, **kwargs):
-        instance.profile.save()
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
-    @receiver(post_save, sender=User)
-    def create_or_update_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(name=instance)
-        instance.profile.save()
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(name=instance)
+    instance.profile.save()
+
+
+
 
 class Neighbourhood(models.Model):
-    name = models.CharField(max_lenth=100)
+    name = models.CharField(max_length=100)
     location = models.CharField(max_length=150)
     image = models.ImageField(upload_to='hood_images/', blank=True, max_length=None)
 
     class Meta:
         db_table = 'neighbourhoods'
         ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name}"
+
+    @classmethod
+    def search_hood(cls, searchTerm):
+        hoods = cls.objects.filter(name__icontains=searchTerm)
+        return hoods
+
+
+
+
+class Updates(models.Model):
+    title = models.CharField(max_length=250)
+    notification = models.CharField(max_length=170)
+    tag = models.CharField(max_length=250)
+    editor = models.ForeignKey(User, on_delete=models.CASCADE)
+    estate = models.ForeignKey('Neighbourhood', on_delete=models.CASCADE, blank=True, null=True)
+    up_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    location = models.CharField(max_length=250)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'businesses'
+        ordering = ['-name']
+
+    def __repr__(self):
+        return f'{self.name}'
+
+    @classmethod
+    def search_biz(cls, searchTerm):
+        biz = cls.objects.filter(name__icontains=searchTerm)
+        return biz
+
+
+
+class EmergencyContact(models.Model):
+    name = models.CharField(max_length=250)
+    contact = models.CharField(max_length=250)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'e_contacts'
+        ordering = ['-name']
+
+    def __repr__(self):
+        return f'{self.name}'
+
+    @classmethod
+    def search_emergencies(cls, searchTerm):
+        emergencies = cls.objects.filter(name__icontains=searchTerm)
+        return emergencies
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(max_length=250)
+    tag = models.CharField(max_length=250)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    hood = models.ForeignKey('Neighbourhood', on_delete=models.CASCADE, related_name='hoods', default=1)
+
+    class Meta:
+        db_table = 'posts'
+        ordering = ['-title']
+
+    def __repr__(self):
+        return f'{self.title}'
+
+    @classmethod
+    def search_posts(cls, searchTerm):
+        posts = cls.objects.filter(title__icontains=searchTerm)
+        return posts
+
+
+
+
+class MyHoodmailer(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField
