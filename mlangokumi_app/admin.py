4d92628eb594cd050712a2f8788c223b90079943
John-Osiko <johnmaxosiko@gmail.com>
2020-11-02 01:12:07 +0300

    Registered model classes

--- mlangokumi_app/admin.py
+++ mlangokumi_app/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import Profile, Neighbourhood, Updates, Business, EmergencyContact, Post
 
 # Register your models here.
 class ProfileInline(admin.StackedInline):
@@ -30,3 +30,9 @@ class CustomUserAdmin(UserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
+admin.site.register(Updates)
+admin.site.register(Business)
+admin.site.register(Neighbourhood)
+admin.site.register(EmergencyContact)
+admin.site.register(Post)
